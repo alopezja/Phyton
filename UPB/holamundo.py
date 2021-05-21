7a4af31db1f736fddc22397bb21ba31e436ac182
Alex López <alopezja@gmail.com>
2021-05-20 20:07:03 -0500

    Clase May 20

--- UPB/holamundo.py
+++ UPB/holamundo.py
@@ -4,3 +4,44 @@ print ("Hola","Adiós")
 print ("Hola",'Adión')
 print ("Hola",end=" este es mi ")
 print ("Mundo")
+
+c = 1
+print (type(c)) 
+
+a=1;b=1.2;c="prueba"
+
+print (a)
+print (b)
+print (c)
+
+#Este es un comentario
+"""Esta es 
+un comentario 
+de varias líneas"""
+
+#Ejemplo 1
+x = 1
+y = 2
+print ("El resultado de la operación es: ",x + y)
+
+#Ejemplo 2
+x = 1
+y = 1
+suma = x+y
+texto = "El resultado de la operacion es = "
+
+print (texto,suma)
+
+#Ejemplo 3
+p="resultado"
+x=2;y=6
+print(p,x+y)
+
+#Ejemplo 4
+x=1 
+y = 1 
+suma = x + y
+suma = suma + y
+p = "El resultado de la operacón = " 
+resultado = p + str(suma)
+print(resultado)
