61485a470e71c0ac0efea640e9e453634e3bf030
Alex López <alopezja@gmail.com>
2021-05-30 21:01:30 -0500

    reto1

--- UPB/Retos semanales/reto1.py
+++ UPB/Retos semanales/reto1.py
@@ -1,5 +1,27 @@
+from os import system
 print("Bienvenido al sistema de ubicación para zonas públicas WIFI”")
-
-usuario = input("Digite usuario: ")
-contraseña = input("Digite contraseña: ")
-
+try:
+    usuario = int(input("Digite el usuario: "))
+    if usuario == 51634:
+        system("clear")
+        contraseña = int(input("Digite la contraseña: "))
+        if contraseña == 43615:
+            system("clear")
+            termino1=634
+            termino2=(((1+2)*2)-3)
+            captcha = str(print(termino1," + ",termino2,"=",))
+            valor=str(input())
+            if valor==str((termino1+termino2)):
+                system("clear")
+                print("Sesión iniciada")
+            else:
+                system("clear")
+                print("Error")
+        else:
+            system("clear")
+            print("Error")
+    else:
+        system("clear")
+        print("Error")
+except:
+    print("Error")
