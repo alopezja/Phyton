c5726bc23dac778ced84482b9bc7dabccb73ea43
Alex López <alopezja@gmail.com>
2021-06-02 20:53:02 -0500

    Jun 2

---
+++ UPB/listas.py
@@ -0,0 +1,15 @@
+# Creación de listas
+"""
+alex = ["Alex",33,True,1.75]
+print(type(alex))
+"""
+########################
+"""
+lenguajes = ["Python","C++","C#","PHP","Java"]
+print(lenguajes[-3])
+"""
+#######################
+amigos = ["Juan", "Pedro", "Carlos"]
+for amigo in amigos:
+    print("Feliz año nuevo",amigo)
+print ("Terminado")
