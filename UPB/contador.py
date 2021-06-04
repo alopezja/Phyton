98e21cd5f8f9b0be2480a054d82623cb23c1efc1
Alex López <alopezja@gmail.com>
2021-06-04 18:17:34 -0500

    Jun 3

---
+++ UPB/contador.py
@@ -0,0 +1,6 @@
+from os import system
+system("clear")
+contador = 0
+for valor in [16,23,34,41,57]:
+    contador = contador + 1
+print ("Las iteraciones de la lista son:",contador)
--- UPB/listas.py
+++ UPB/listas.py
@@ -1,4 +1,6 @@
 # Creación de listas
+from os import system
+system("clear")
 """
 alex = ["Alex",33,True,1.75]
 print(type(alex))
@@ -9,7 +11,22 @@ lenguajes = ["Python","C++","C#","PHP","Java"]
 print(lenguajes[-3])
 """
 #######################
+"""
 amigos = ["Juan", "Pedro", "Carlos"]
 for amigo in amigos:
     print("Feliz año nuevo",amigo)
 print ("Terminado")
+"""
+#######################
+"""
+lista = ["Juan", "Antonio", "Pedro", "Luis"]
+for nombre in lista:
+    print(nombre)
+"""
+#########################
+
+for año in range (2001, 2013):
+    print ("Informes del año:", año)
+
+for año in range (2013, 2001, -1):
+    print ("Informes del año:", año)
